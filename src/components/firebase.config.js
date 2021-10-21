c9df2df85a8ba90352f1a1ac7eaabfd2775fec36
Zombie-Muse <gjmccormack@cpp.edu>
2021-10-21 15:57:59 -0700

    set up demo on firebase

--- src/components/firebase.config.js
+++ src/components/firebase.config.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_KEY_DEMO,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN_DEMO,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_DEMO,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_DEMO,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID_DEMO,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID_DEMO,
 };
 
 firebase.initializeApp(firebaseConfig);
