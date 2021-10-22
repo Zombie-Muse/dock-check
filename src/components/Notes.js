73cfc99d7da62b41412baac803b72b570c6dfd06
Zombie-Muse <gjmccormack@cpp.edu>
2021-10-21 19:02:53 -0700

    Updates Notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -10,9 +10,8 @@ const Notes = () => {
         </li>
         <li>Prefix is now "trailer type."</li>
         <li>
-          I'm having some issues with the trailer status displaying in the main
-          row. It will eventually be color coded for "empty," "breakout," and
-          "arrive."
+          I'm having some issues with the color coded for "empty," "breakout,"
+          and "arrive" feature. It's a little buggy at times. I'm working on it.
         </li>
         <li>
           Door History still does not currently work. It's more of a placeholder
@@ -24,6 +23,19 @@ const Notes = () => {
           <em>
             (i.e. you can make a note that there's a GSDS order in the trailer,
             or if the current trailer is red tagged, etc)
+          </em>
+        </li>
+        <li>
+          I think I will change the breakout and arrive checkboxes to a toggle
+          switch instead. Once the trailer is arrived, it will automatically
+          switch to breakout. Status won't display multiple inputs (it will only
+          display the right-most in the checkbox row).
+        </li>
+        <li>
+          I'm also working on search trailer functionality and filtered views.
+          <em>
+            i.e. you can display all trailers that need to be arrived or all
+            breakouts.
           </em>
         </li>
       </ul>
